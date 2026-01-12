827b5bf120d2a9a42deb62986648907829368c56
poojprab <prabu.p@northeastern.edu>
2026-01-12 09:17:59 -0500

    Removed button + use client as per the Piazza note

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
@@ -258,13 +257,6 @@ export default function Lab1() {
           </textarea>
 
           <h5 id="wd-buttons">Buttons</h5>
-          <button
-            type="button"
-            onClick={() => alert("Life is Good!")}
-            id="wd-all-good"
-          >
-            Hello World!
-          </button>
           <h5 id="wd-radio-buttons">Radio buttons</h5>
             <label>Favorite movie genre:</label><br />
 
