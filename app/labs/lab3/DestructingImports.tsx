865b30fe17b76c91f0b40f82411650edcdef2630
poojprab <prabu.p@northeastern.edu>
2026-02-03 19:38:32 -0500

    Finished Lab 3

---
+++ app/labs/lab3/DestructingImports.tsx
@@ -0,0 +1,44 @@
+import Math, { add, subtract, multiply, divide } from "./Math";
+import * as Matematica from "./Math";
+export default function DestructingImports() {
+  return (
+    <div id="wd-destructuring-imports">
+      <h2>Destructing Imports</h2>
+      <table className="table table-sm">
+        <thead>
+          <tr>
+            <th>Math</th>
+            <th>Matematica</th>
+            <th>Functions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Math.add(2, 3) = {Math.add(2, 3)}</td>
+            <td>Matematica.add(2, 3) =
+                {Matematica.add(2, 3)}</td>
+            <td>add(2, 3) = {add(2, 3)}</td>
+          </tr>
+          <tr>
+            <td>Math.subtract(5, 1) = {Math.subtract(5, 1)}</td>
+            <td>Matematica.subtract(5, 1) =
+                {Matematica.subtract(5, 1)}</td>
+            <td>subtract(5, 1) = {subtract(5, 1)}</td>
+          </tr>
+          <tr>
+            <td>Math.multiply(13, 1) = {Math.subtract(13, 1)}</td>
+            <td>Matematica.multiply(13, 1) =
+                {Matematica.multiply(13, 1)}</td>
+            <td>multiply(13, 1) = {multiply(13, 1)}</td>
+          </tr>
+          <tr>
+            <td>Math.divide(20, 4) = {Math.divide(20, 4)}</td>
+            <td>Matematica.divide(20, 4) =
+                {Matematica.divide(20, 4)}</td>
+            <td>divide(20, 4) = {divide(20, 4)}</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+    </div>
+);}
--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,6 +1,82 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import TodoList from "./todos/TodoList";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import Classes from "./Classes";
+import ClientComponentDemo from "./ClientComponentDemo";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInLine";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import House from "./House";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import PathParameters from "./PathParameters";
+import ServerComponentDemo from "./ServerComponentDemo";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreader";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+
 export default function Lab3() {
-  return (
-    <div>
-      <h2>Lab 3</h2>
+  console.log('Hello World!');
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+    <TodoList />
+    <AddingAndRemovingToFromArrays />
+    <ArrayIndexAndLength />
+    <ArrowFunctions />
+    <Classes />
+    <ClientComponentDemo />
+    <ConditionalOutputIfElse />
+    <ConditionalOutputInline />
+    <Destructing />
+    <DestructingImports />
+    <FilterFunction />
+    <FindFunction />
+    <FindIndex />
+    <ForLoops />
+    <FunctionDestructing />
+    <House />
+    <ImpliedReturn />
+    <JsonStringify />
+    <LegacyFunctions />
+    <MapFunction />
+    <PathParameters />
+    <ServerComponentDemo />
+    <SimpleArrays />
+    <Spreading />
+    <Styles />
+    <TemplateLiterals />
     </div>
-);}
+  );
+}
