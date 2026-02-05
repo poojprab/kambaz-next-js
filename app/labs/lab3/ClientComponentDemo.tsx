dd1b22cfd0fb6fafc21c0eebf8055d19c0b7c70f
poojprab <prabu.p@northeastern.edu>
2026-02-05 12:39:08 -0500

    I think its over

--- app/labs/lab3/ClientComponentDemo.tsx
+++ app/labs/lab3/ClientComponentDemo.tsx
@@ -1,11 +1,12 @@
 "use client";
-
+import { useEffect } from "react";
 
 export default function ClientComponentDemo() {
- // This runs ONLY in the browser
- alert(
-   "Hello! This alert runs in the browser because this is a Client Component."
- );
+  useEffect(() => {
+    alert(
+      "Hello! This alert runs in the browser because this is a Client Component."
+    );
+  }, []);
 
 
  return (
