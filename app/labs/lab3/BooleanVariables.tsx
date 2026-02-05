de614d659a11128b51f6e2edf95732ea1497bad4
poojprab <prabu.p@northeastern.edu>
2026-02-05 12:18:57 -0500

    fixed the error

--- app/labs/lab3/BooleanVariables.tsx
+++ app/labs/lab3/BooleanVariables.tsx
@@ -5,7 +5,7 @@ export default function BooleanVariables() {
     const true2 = true1 || false1;
     const true3 = !false2;
     const true4 = numberVariable === 123; // always use === not ==
-    const true5 = floatingPointNumber !== 321.432;
+    // const true5 = floatingPointNumber !== 321.432;
     const false3 = numberVariable < 100;
     return (
     <div id="wd-boolean-variables">
@@ -16,7 +16,7 @@ export default function BooleanVariables() {
         true2     = {true2 + ""}     <br />
         true3     = {true3 + ""}     <br />
         true4     = {true4 + ""}     <br />
-        true5     = {true5 + ""}     <br />
+        {/* true5     = {true5 + ""}     <br /> */}
         false3    = {false3 + ""}    <hr />
     </div>
     );
