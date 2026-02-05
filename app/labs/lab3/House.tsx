95a207fcfeceb42f74b4edbed8c0f20dbb1ea7b7
poojprab <prabu.p@northeastern.edu>
2026-02-05 12:31:17 -0500

    attempting to fix issues again

--- app/labs/lab3/House.tsx
+++ app/labs/lab3/House.tsx
@@ -6,12 +6,16 @@ export default function House() {
       street: "Via Roma", city: "Roma", state: "RM", zip: "00100",  country: "Italy", },
     owners: ["Alice", "Bob"],
   };
-  console.log(house);
+  console.log(house.bedrooms);
   return (
     <div id="wd-house">
       <h4>House</h4>
       <h5>bedrooms</h5>      {house.bedrooms}
       <h5>bathrooms</h5>     {house.bathrooms}
+      <h5>squareFeet</h5>     {house.squareFeet}
+      <h5>address</h5>     {house.address.street} {house.address.state} {house.address.zip}
+      {house.address.country}
+      <h5>owners</h5> {house.owners[0]} {house.owners[1]}
       <h5>Data</h5>
       <pre>{JSON.stringify(house, null, 2)}</pre>
       <hr />
