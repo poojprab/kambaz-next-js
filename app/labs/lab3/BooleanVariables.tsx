0999037db715488b42483da1091b734bc5c03599
poojprab <prabu.p@northeastern.edu>
2026-02-05 12:23:23 -0500

    fixed again :(

--- app/labs/lab3/BooleanVariables.tsx
+++ app/labs/lab3/BooleanVariables.tsx
@@ -1,5 +1,6 @@
 export default function BooleanVariables() {
-    const numberVariable = 123, floatingPointNumber = 234.345;
+    const numberVariable = 123;
+    // , floatingPointNumber = 234.345;
     const true1 = true, false1 = false;
     const false2 = true1 && false1;
     const true2 = true1 || false1;
