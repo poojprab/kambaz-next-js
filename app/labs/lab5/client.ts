538c250b9c12228e8104fba118853829973d4c32
poojprab <your-prabup-cs-email@example.com>
2026-03-23 14:06:29 -0400

    Added missing client file

--- app/labs/lab5/client.ts
+++ app/labs/lab5/client.ts
@@ -1,46 +1,45 @@
 import axios from "axios";
+
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
-export const fetchWelcomeMessage = async () => {
-  const response = await axios.get(`${HTTP_SERVER}/lab5/welcome`);
-  return response.data;
-};
-const ASSIGNMENT_API = `${HTTP_SERVER}/lab5/assignment`;
-export const fetchAssignment = async () => {
-  const response = await axios.get(`${ASSIGNMENT_API}`);
-  return response.data;
-};
-export const updateTitle = async (title: string) => {
-  const response = await axios.get(`${ASSIGNMENT_API}/title/${title}`);
-  return response.data;
-};
-const TODOS_API = `${HTTP_SERVER}/lab5/todos`;
+const LAB5_API = `${HTTP_SERVER}/lab5`;
+
 export const fetchTodos = async () => {
-  const response = await axios.get(TODOS_API);
-  return response.data;
+  const { data } = await axios.get(`${LAB5_API}/todos`);
+  return data;
 };
+
 export const createNewTodo = async () => {
-  const response = await axios.get(`${TODOS_API}/create`);
-  return response.data;
+  const { data } = await axios.get(`${LAB5_API}/todos/create`);
+  return data;
 };
-export const postNewTodo = async (todo: {
-  title: string;
-  completed: boolean;
-}) => {
-  const response = await axios.post(`${TODOS_API}`, todo);
-  return response.data;
+
+export const postNewTodo = async (todo: object) => {
+  const { data } = await axios.post(`${LAB5_API}/todos`, todo);
+  return data;
 };
+
 export const removeTodo = async (todo: { id: number }) => {
-  const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
-  return response.data;
+  const { data } = await axios.get(`${LAB5_API}/todos/${todo.id}/delete`);
+  return data;
 };
+
 export const deleteTodo = async (todo: { id: number }) => {
-  const response = await axios.delete(`${TODOS_API}/${todo.id}`);
-  return response.data;
+  await axios.delete(`${LAB5_API}/todos/${todo.id}`);
 };
+
 export const updateTodo = async (todo: {
   id: number;
   [key: string]: unknown;
 }) => {
-  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
-  return response.data;
+  await axios.put(`${LAB5_API}/todos/${todo.id}`, todo);
+};
+
+export const fetchAssignment = async () => {
+  const { data } = await axios.get(`${LAB5_API}/assignment`);
+  return data;
+};
+
+export const updateTitle = async (title: string) => {
+  const { data } = await axios.get(`${LAB5_API}/assignment/title/${title}`);
+  return data;
 };
